b1de6f09b97955d6a52aa0f3a27859303b26c5ad
jayanth-parthsarathy <jparthaa@gmail.com>
2023-08-13 21:31:34 +0530

    Setup prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,29 @@
 import express, { Express, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
 import dotenv from 'dotenv';
-
 dotenv.config();
+
+
 
 const app: Express = express();
 const port = process.env.PORT;
+const prisma = new PrismaClient()
+async function main() {
+  app.get('/', async (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+  });
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  });
+}
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
