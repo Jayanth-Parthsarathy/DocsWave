968b0c67d3195eb50e81560d4430107f2439005b
jayanth-parthsarathy <jparthaa@gmail.com>
2023-08-14 11:42:18 +0530

    Remove redundant code and setup tailwind and socket io

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,35 @@
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
 import { PrismaClient } from '@prisma/client'
 import dotenv from 'dotenv';
+import cors from "cors"
+import http from "http";
+import { Server } from "socket.io";
 import authRouter from './routes/authRoutes';
 import userRouter from './routes/userRoutes';
 import documentRouter from './routes/documentRoutes';
 dotenv.config();
-
-
-
-const app: Express = express();
-const port = process.env.PORT;
-app.use(express.json())
 const prisma = new PrismaClient()
+
+
 async function main() {
+  const app = express();
+  const port = process.env.PORT || 5000;
+  const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    },
+  });
+  app.use(express.json())
+  app.use(cors())
   app.use('/api/auth', authRouter)
   app.use('/api/user', userRouter)
   app.use('/api/document', documentRouter)
-  app.listen(port, () => {
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+  server.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
   });
 }
